62c0bf95269fb0bf0c9fabaf8ee0b9415c43d705
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2020-06-30 08:39:34 -0300

    Resolvido maior parte do desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,26 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    let { sum: income } = await this.createQueryBuilder('transactions')
+      .select('SUM(value)', 'sum')
+      .where('transactions.type = :type', { type: 'income' })
+      .getRawOne();
+
+    let { sum: outcome } = await this.createQueryBuilder('transactions')
+      .select('SUM(value)', 'sum')
+      .where('transactions.type = :type', { type: 'outcome' })
+      .getRawOne();
+
+    income = Number(income);
+    outcome = Number(outcome);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 }
 
